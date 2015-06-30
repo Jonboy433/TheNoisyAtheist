555c1b94a432c7d5dc8ced5ae8bc81d90fda97af
John <jonboy433@hotmail.com>
2015-06-30 00:59:13 -0400

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:user_id, :password]
+  validates :user_id,
+  	:presence => true,
+  	:uniqueness => {
+  		:case_sensitive => false
+  	}
+  validates :email,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+    }
+ 
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["user_id = :value", { :value => login.downcase }]).first
+      else
+        where(conditions.to_hash).first
+      end
+  end
+end
