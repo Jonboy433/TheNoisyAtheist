f58b42cc46d1f759af4f64bb84961783386a785a
John Greco <jonboy433@hotmail.com>
2016-06-16 10:49:15 -0400

    Added basic user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    #Display user profile with all relevant data
+    def show
+        find_user
+        render 'show'
+    end
+
+    def update
+    end
+
+    private
+    def find_user
+        @user = User.find_by_user_id(params[:id])
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,4 @@
 class Comment < ActiveRecord::Base
 	validates :content, :user_id, :blog_id, presence: true
-	
-	belongs_to :user
-	belongs_to :blog
+
 end
