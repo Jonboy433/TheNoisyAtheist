cda6261ea2f7ab6d23622bd11233b827e82109c4
John Greco <jonboy433@hotmail.com>
2016-06-23 15:11:15 -0400

    Update the show page: Blog owner name is now a link that takes you to their user profile

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,4 +3,8 @@ module BlogsHelper
     def edit_blog_url(displayID)
         sprintf("/blogs/%s/edit",displayID)
     end
+
+    def user_profile_url(userID)
+        sprintf("/users/%s",userID)
+    end
 end
