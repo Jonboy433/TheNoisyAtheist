cb210092f68b5b72ade13fa76a2533338e2d27c6
John Greco <jonboy433@hotmail.com>
2016-06-24 23:39:06 -0400

    Added method to redirect to current page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   protected
+
+  def after_sign_in_path_for(resource)
+    #Redirect back to the page they were on prior to login
+    request.referrer
+  end
+
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_id, :email, :password, :password_confirmation, :remember_me)}
   	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me) }
