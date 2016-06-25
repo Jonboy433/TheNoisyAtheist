979e897cd3894d109b60a4a121b79babba2c7921
John Greco <jonboy433@hotmail.com>
2016-06-25 00:34:09 -0400

    Fixed the error handling for async comments and added js to add error messages to the blogs show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,15 @@ class CommentsController < ApplicationController
 		@comment = @blog.comments.build(comment_params)
 		@comment.blog = @blog
     	@comment.user = current_user
-		
+
 		respond_to do |format|
 			if @comment.save
+				@saved = true #storing bool for js.erb
     			format.html { redirect_to blog_path(@blog.display_id) }
 				format.js { render status: :created }
+			else
+				@saved = false
+				format.js { render status: :unprocessable_entity}
 			end
 		end
 	end
