9303651e21e81cc679e873f04a769dce0a859fdf
John Greco <jonboy433@hotmail.com>
2016-06-29 18:07:40 -0400

    Added some basic client-side validation on the login form: If either UID or password field is blank stop the login process and display error message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,15 @@
 
 
 $(document).ready(function(){
-    
+
     $("#submitLogin").click(function(event){
-
-        $("#loginMsg").remove();
         event.preventDefault();
-        var inputs = $("#loginForm").serialize();
+        
+        var inputs = $("#loginForm").serializeArray();
+        var uidLength = $("#formUID").val().length;
+        var pwdLength = $("#formPWD").val().length;
 
+        if (formValid(uidLength,pwdLength)) {
             $.ajax({
                 url: '/users/sign_in',
                 type: 'POST',
@@ -35,24 +37,50 @@ $(document).ready(function(){
                     200: function(data){
                         $(".loginMsg").remove();
                         //Reload page after login
-                        location.reload();
-                        
+                        location.reload();                      
                     },
                     201: function(data){
                         $(".loginMsg").remove();
                         //Reload page after login
-                        location.reload();
-                        
+                        location.reload();                   
                     },
                     401: function(data){
                         //Add error message to login form for invalid credentials
                         var errorMsg = '<div class="alert alert-danger" id="loginMsg" role="alert">' + data.responseJSON.errors[0] + '</div>';
-                        $(".dropdown-menu").prepend(errorMsg);
+                        // $(".dropdown-menu").prepend(errorMsg);
+                        $("#loginForm").prepend(errorMsg);
                     },
                     422: function(data){
                         console.log(data.responseText);
                     }
                 }
-            });
-     });
+            });//End AJAX 
+        } //End If validated
+
+        else {
+            $("#loginMsg").remove();
+            var errorMsg = '<div class="alert alert-danger" id="loginMsg" role="alert">Required fields missing</div>';
+            $("#loginForm").prepend(errorMsg);
+        }
+
+        }); //End click function
+
+
+        $(".dropdown-toggle").click(function(){
+            //Clear the failure message whenever dropdown is opened
+            $("#loginMsg").remove();
+        });
+
+
 });
+
+function formValid(uLength, pLength) {
+    //Implement basic validation for now
+    if (uLength > 0 && pLength > 0) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
